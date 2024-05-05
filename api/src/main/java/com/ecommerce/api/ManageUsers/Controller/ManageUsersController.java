242f1725c54c8b7a8e52f5cf6405fca689a0d526
Oumaima-Daoudi <daoudioumaima300@gmail.com>
2024-05-05 15:38:23 +0100

    add some changes

--- api/src/main/java/com/ecommerce/api/ManageUsers/Controller/ManageUsersController.java
+++ api/src/main/java/com/ecommerce/api/ManageUsers/Controller/ManageUsersController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -140,6 +141,24 @@ public class ManageUsersController {
         userRepository.save(user);
         return ResponseEntity.ok().build();
     }
+
+
+
+    @PostMapping("/adminProfile")
+    public ResponseEntity<?> getAdminProfile(@RequestBody long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            if (user.getRole().equals("ADMIN")) {
+                return ResponseEntity.ok(user);
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("you are unauthorized to access this page");
+            }
+        } else {
+            return ResponseEntity.notFound().build(); 
+        }
+    }
+    
 
 
 
